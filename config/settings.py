aa32bfbfa6822eaf5fc0217326d5bf4959111f5f
mahadley <mark.hadley1@hotmail.com>
2021-05-17 13:39:15 -0700

    first update

--- config/settings.py
+++ config/settings.py
@@ -120,7 +120,16 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_URL = '/images/'
+
+STATICFIELDS_DIRS = [
+    BASE_DIR, 'static'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Completing the setting
+
